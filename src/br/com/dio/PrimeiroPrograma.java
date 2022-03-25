10d9c0dd91db0d16a8d847801eb6eb1047600a32
aguinaldomneto <aguinaldomneto@gmail.com>
2022-03-25 19:59:34 -0300

    2

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -12,9 +12,7 @@ public class PrimeiroPrograma {
 		System.out.println(gato);
 		System.out.println(livros);
 		System.out.println(receitas);
-	/*	int a = 2;
-		int b = 5;	
-		System.out.println("Hello World! " + (a + b) );*/
+
 	} 
 
 }
