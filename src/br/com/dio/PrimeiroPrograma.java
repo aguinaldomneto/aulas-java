cf9d269ab6b2c16c363f890791f9e7bab606e195
aguinaldomneto <aguinaldomneto@gmail.com>
2022-03-25 19:56:37 -0300

    primeiro Commit.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -12,7 +12,6 @@ public class PrimeiroPrograma {
 		System.out.println(gato);
 		System.out.println(livros);
 		System.out.println(receitas);
-		123
 	/*	int a = 2;
 		int b = 5;	
 		System.out.println("Hello World! " + (a + b) );*/
