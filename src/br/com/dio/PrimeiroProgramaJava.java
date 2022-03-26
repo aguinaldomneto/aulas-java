168dca0393a8325f43fc7ebab803aa358272a86c
aguinaldomneto <aguinaldomneto@gmail.com>
2022-03-25 23:09:25 -0300

    class livro

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -1,14 +1,50 @@
 package br.com.dio;
 
-import br.com.dio.model.gato;
+/*import br.com.dio.model.gato;*/
 
 public class PrimeiroProgramaJava {
     public static void main(String[] args) {
     Gato gato = new Gato();
+        Livro livro1 = new Livro("A solução de tudo", "300");
+        System.out.println(livro1);
 
         System.out.println(gato);
         /*        int a = 3;
         int b = 4;
         System.out.println("ola mundo, a + b = " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
