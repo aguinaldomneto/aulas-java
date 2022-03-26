035f2f6bddca6cf07d55898440f201601708148c
aguinaldomneto <aguinaldomneto@gmail.com>
2022-03-25 22:18:25 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+    Gato gato = new Gato();
+
+        System.out.println(gato);
+        /*        int a = 3;
+        int b = 4;
+        System.out.println("ola mundo, a + b = " + (a + b));*/
+    }
+}
